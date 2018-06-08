63486c4edfe3ad8b850fa96682bd61f81c2758ca
mpritzkoleit <max.pritzkoleit@tu-dresden.de>
2018-06-08 13:23:08 +0200

    new path planner

--- SystemSimulation-ODE/sim/Planner.py
+++ SystemSimulation-ODE/sim/Planner.py
@@ -17,22 +17,25 @@ class Planner(object):
 
 
 class PolynomialPlanner(Planner):
-    def evaluate(self,t):
+    def eval(self,t):
         # TODO: check if t is scalar
         if t<self.t0:
             y = self.y0
         elif t>self.tT:
             y = self.yT
         else:
-            y = 1
+            y = np.dot(self.TMatrix(t),self.coefficients())
         return y
 
     def TMatrix(self, t):
         d = self.d
-        T = np.zeros_like(np.matrix(d,2*d+1))
-        for j in range(0, d):
-            for i in range(0, 2*d+1):
-                T[j,i] = t**(i-j)/math.factorial(i-j)
+        n = d+1
+        m = 2*d+2
+        T = np.zeros([n, m])
+        for i in range(0, m):
+            T[0, i] = t ** i / math.factorial(i)
+        for j in range(1, n):
+            T[j, j:m] = T[0, 0:m-j]
         return T
 
 
@@ -40,9 +43,25 @@ class PolynomialPlanner(Planner):
         # TODO: should be called, when instance is constructed
         t0 = self.t0
         tT = self.tT
-        Y = np.stack(self.y0, self.yT)
-        T = np.stack(self.TMatrix(t0), self.TMatrix(tT))
-        c = np.linalg.inv(T)*Y
+        Y = np.append(self.y0, self.yT)
+        T0 = self.TMatrix(t0)
+        TT = self.TMatrix(tT)
+        T = np.append(T0, TT, axis=0)
+        c = np.linalg.solve(T, Y)
         return c
 
 
+
+y0 = np.array([0.5, 1])
+yT = np.array([1, 0])
+t0 = 0.2
+tT = 1
+d = 1
+pp = PolynomialPlanner(y0,yT,t0,tT,d)
+
+print(pp.TMatrix(t0))
+print(pp.TMatrix(tT))
+print(pp.coefficients())
+print(pp.eval(t0))
+print(pp.eval(tT))
+print(pp.eval(0.5*(tT-t0)))
