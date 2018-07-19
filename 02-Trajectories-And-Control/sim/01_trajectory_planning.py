e4722732840453f75ec5e71ec597ce998e7cd4d1
mpritzkoleit <maxpritzkoleit@aol.com>
2018-07-19 15:18:25 +0200

    finish tutorial 2

--- 02-Trajectories-And-Control/sim/01_trajectory_planning.py
+++ 02-Trajectories-And-Control/sim/01_trajectory_planning.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Planner import PolynomialPlanner, PrototypePlanner, GevreyPlanner
 
-# example 1
+# example 1 - polynomial planner
 YA = np.array([0, 0, 0]) # t = t0
 YB = np.array([1, 0, 0]) # t = tf
 t0 = 0 # start time of transition
 tf = 1 # final time of transition
-tt = np.linspace(t0-1, tf+1, 500) # -1 to 4 in 500 steps
+tt = np.linspace(t0, tf, 100) # -1 to 4 in 500 steps
 d = 2 # smooth derivative up to order d
 yd = PolynomialPlanner(YA, YB, t0, tf, d)
 
@@ -26,7 +26,7 @@ plt.legend([r'$y_d(t)$', r'$\dot{y}_d(t)$',r'$\ddot{y}_d(t)$'])
 plt.xlabel(r't in s')
 plt.grid(True)
 
-# example 2
+# example 2 - prototype planner
 yd2 = PrototypePlanner(YA, YB, t0, tf, d)
 
 # sample the generated trajectory
@@ -40,12 +40,14 @@ plt.legend([r'$y_d(t)$', r'$\dot{y}_d(t)$',r'$\ddot{y}_d(t)$'])
 plt.xlabel(r't in s')
 plt.grid(True)
 
-# example 3
-s = 1.1
-yd3 = GevreyPlanner(YA, YB, t0, tf, d, s)
-
+# example 3 - Gevrey planner
+s1 = 1.1
+s2 = 1.9
+yd3 = GevreyPlanner(YA, YB, t0, tf, d, s1)
+yd4 = GevreyPlanner(YA, YB, t0, tf, d, s2)
 # sample the generated trajectory
 Y3 = yd3.eval_vec(tt)
+Y4 = yd4.eval_vec(tt)
 #plot the trajectory
 plt.figure(3)
 plt.plot(tt, Y3)
@@ -53,4 +55,19 @@ plt.title('Planned trajectory')
 plt.legend([r'$y_d(t)$', r'$\dot{y}_d(t)$',r'$\ddot{y}_d(t)$'])
 plt.xlabel(r't in s')
 plt.grid(True)
+
+plt.figure(4)
+plt.plot(tt, Y3[:,0],tt, Y4[:,0])
+plt.title(r'$y = \varphi^{(0)}_{\sigma,T}(t)$')
+plt.legend([r'$\sigma = 1.1$', r'$\sigma = 1.9$'])
+plt.xlabel(r't in s')
+plt.grid(True)
+
+plt.figure(5)
+plt.plot(tt, Y3[:,1],tt, Y4[:,1])
+plt.title(r'$y = \varphi^{(1)}_{\sigma,T}(t)$')
+plt.legend([r'$\sigma = 1.1$', r'$\sigma = 1.9$'])
+plt.xlabel(r't in s')
+plt.grid(True)
 plt.show()
+
