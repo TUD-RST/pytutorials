0638b535fdf4cab5aa773263bc78a8416663a098
Robert Heedt <robert.heedt@gmail.com>
2019-12-02 13:05:56 +0100

    Add pseudo LTV feedback (linearize at each time step)
    Make feedback mode switchable
    Find state and input for static feedback based on time
    Invert R matrix only once

--- 04-LQR-Design-And-Simulation/sim/01_lqr.py
+++ 04-LQR-Design-And-Simulation/sim/01_lqr.py
@@ -4,6 +4,7 @@ from numpy import cos, sin, tan, pi
 import scipy.linalg as scilin
 import matplotlib.pyplot as plt
 from Planner import PolynomialPlanner
+from typing import Literal
 
 
 class Parameters(object):
@@ -14,8 +15,8 @@ class Parameters(object):
 sim_para = Parameters()  # instance of class Parameters
 sim_para.t0 = 0          # start time
 sim_para.tf = 10         # final time
-sim_para.dt = 0.1       # step-size
-sim_para.x0 = [2.1, 0.1]
+sim_para.dt = 0.1        # step-size
+sim_para.x0 = [-2.1, 0.1] # initial value
 
 # already prepare the time vector because we'll need it very soon
 n_samples = int((sim_para.tf - sim_para.t0) / sim_para.dt) + 1
@@ -58,8 +59,8 @@ R = np.array([[1]])
 
 # trajectory parameters
 traj_para = Parameters()
-traj_para.y0 = 2
-traj_para.yf = -2
+traj_para.y0 = -2
+traj_para.yf = 2
 traj_para.t0 = 0
 traj_para.tf = 10
 
@@ -78,6 +79,8 @@ ud_traj = x2d_dot + np.square(x1d)
 
 
 # -- END SYSTEM SPECIFIC PART --
+
+R_inv = scilin.inv(R)
 
 
 # solve matrix riccati ODE
@@ -98,13 +101,16 @@ def full_to_triu(full):
     return full[mask]
 
 
+# Get initial value S for matrix Riccati ODE by solving algebraic Riccati equation
 A_f, B_f = system_matrices(t_traj[-1], xd_traj[-1], ud_traj[-1], sys_para)
 S = scilin.solve_continuous_are(A_f, B_f, Q, R)
+
 Ptilde_triu_traj = np.empty((n_samples, int(sys_para.n*(sys_para.n + 1)/2)))
 Ptilde_triu_traj[0, :] = full_to_triu(S)
 
 K_traj = np.empty((n_samples, sys_para.m, sys_para.n))
 
+# get trajectories for P and K via numerical integration
 for i_tau in range(n_samples):
     i_t = n_samples - 1 - i_tau
     t_i = t_traj[i_t]
@@ -115,35 +121,48 @@ for i_tau in range(n_samples):
     Ptilde_triu_i = Ptilde_triu_traj[i_tau]
     Ptilde_i = triu_to_full(Ptilde_triu_i)
 
-    K_traj[i_t] = scilin.inv(R) @ B_i.T @ Ptilde_i
+    K_traj[i_t] = R_inv @ B_i.T @ Ptilde_i
 
     if i_tau < n_samples - 1:
-        dPtilde_dtau = - Ptilde_i @ B_i @ scilin.inv(R) @ B_i.T @ Ptilde_i + Ptilde_i @ A_i + A_i.T @ Ptilde_i + Q
+        dPtilde_dtau = - Ptilde_i @ B_i @ R_inv @ B_i.T @ Ptilde_i + Ptilde_i @ A_i + A_i.T @ Ptilde_i + Q
         dPtilde_dtau_triu = full_to_triu(dPtilde_dtau)
 
         Ptilde_triu_traj[i_tau + 1] = Ptilde_triu_i + sim_para.dt * dPtilde_dtau_triu  # one Euler step
 
 # compute static LQR feedback
-t_static = 10
-x_static = xd_traj[-1]
-ud_static = ud_traj[-1]
+t_static = 5
+i_static = 0
+while i_static < len(t_traj) - 1 and t_traj[i_static] < t_static:  # find index of that time in time vector
+    i_static += 1
+x_static = xd_traj[i_static]
+ud_static = ud_traj[i_static]
 A_static, B_static = system_matrices(t_static, x_static, ud_static, sys_para)
 P_static = scilin.solve_continuous_are(A_static, B_static, Q, R)
-K_static = scilin.inv(R) * B_static.T @ P_static
+K_static = R_inv * B_static.T @ P_static
 
 # main simulation loop
 x_traj = np.empty((n_samples, sys_para.n))
 x_traj[0] = sim_para.x0
 u_traj = np.empty((n_samples, sys_para.m))
 
+FeedbackMode = Literal["LTV", "LTI", "pseudoLTV"]
+feedback_mode: FeedbackMode = "LTV"
+
 for i in range(n_samples):
+    t_i = t_traj[i]
     x_i = x_traj[i]
 
     xd_i = xd_traj[i]
     ud_i = ud_traj[i]
 
-    K_i = K_traj[i]
-    #K_i = K_static
+    if feedback_mode == "LTV":
+        K_i = K_traj[i]
+    elif feedback_mode == "LTI":
+        K_i = K_static
+    elif feedback_mode == "pseudoLTV":
+        A_i, B_i = system_matrices(t_i, xd_i, ud_i, sys_para)
+        P_i = scilin.solve_continuous_are(A_i, B_i, Q, R)
+        K_i = R_inv * B_i.T @ P_i
 
     u_i = ud_i - K_i @ (x_i - xd_i)
     dxdt_i = system_rhs(t_traj[i], x_i, u_i, sys_para)
